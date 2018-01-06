ee0c041a079113ddf363c1b01d00ae344e477dab
brett.g.hays <brett.g.hays.coding@gmail.com>
2018-01-06 10:44:09 -0700

    Added google font and header component

--- weather/src/App.js
+++ weather/src/App.js
@@ -1,18 +1,16 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+import Header from './components/Header/header'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className="header"><Header /></div>
+        <div className="search">Search Here</div>
+        <div className="forecast">Enter City</div>
+        <div className="recent">Recent Searches</div>
       </div>
     );
   }
---
+++ weather/src/components/Header/header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './header.css';
+
+const Header = Header => {
+    return(
+        <div className="header">
+            <h1>Weather App</h1>
+        </div>
+    )
+}
+
+export default Header
